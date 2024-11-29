e87e97cc3370bc35a7480bbc3d325344c49d0624
Adrian Pascu <mail@adipascu.ro>
2024-11-29 03:30:47 +0100

    Log battery temperature

--- app/src/main/java/ro/adipascu/batterymanager/App.kt
+++ app/src/main/java/ro/adipascu/batterymanager/App.kt
@@ -1,13 +1,26 @@
 package ro.adipascu.batterymanager
 
 import android.app.Application
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.BatteryManager
+import android.util.Log
 
 lateinit var batteryManager: BatteryManager
+
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         batteryManager =
             getSystemService(BATTERY_SERVICE) as BatteryManager
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(c: Context?, intent: Intent) {
+                val temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10.0
+                Log.d("BatteryManager", "Battery temperature changed: $temperature°C")
+            }
+
+        }, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
     }
 }
