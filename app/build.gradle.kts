c6202a5a83e25938ac6a1aad0645d19157224b9c
Adrian Pascu <mail@adipascu.ro>
2024-11-29 03:57:03 +0100

    Set minSdk to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ro.adipascu.batterymanager"
-        minSdk = 23
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
